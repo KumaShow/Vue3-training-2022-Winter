c551df63272d98503484eeaae4c5d3834fa16471
KumaShow <kumadth@gmail.com>
2023-02-06 18:30:10 +0800

    將刪除、新增、修改的 modal 拆元件使用

--- Week4/js/adminProducts.js
+++ Week4/js/adminProducts.js
@@ -1,8 +1,9 @@
 import { createApp } from "https://unpkg.com/vue@3/dist/vue.esm-browser.js";
 import pagination from "./components/pagination.js";
-// import deleteModal from "./components/deleteModal.js";
+import deleteModal from "./components/deleteModal.js";
+import productModal from "./components/productModal.js";
 
-let productModal = null;
+// let productModal = null;
 let delProductModal = null;
 
 const app = createApp({
@@ -19,7 +20,8 @@ const app = createApp({
   },
   components: {
     pagination,
-    // deleteModal,
+    deleteModal,
+    productModal,
   },
   mounted() {
     // 取出 token，並設定 axios 預設 header
@@ -75,79 +77,27 @@ const app = createApp({
         this.tempProduct = {
           imagesUrl: [],
         };
-        productModal.show();
+        this.$refs.productModal.openModal();
       } else if (status === "edit") {
         this.isNew = false;
         this.tempProduct = { ...product };
-        productModal.show();
+        this.$refs.productModal.openModal();
       } else if (status === "delete") {
         this.tempProduct = { ...product };
-        delProductModal.show();
+        this.$refs.delProductModal.openModal();
       }
     },
-  },
-});
 
-app.component("productModal", {
-  template: "#productModal",
-  props: ["tempProduct", "isNew"],
-  mounted() {
-    // 綁定 Modal DOM
-    productModal = new bootstrap.Modal(document.getElementById("productModal"));
-  },
-  methods: {
-    updateProduct() {
-      // 預設為更新產品
-      let url = `${apiUrl}/api/${apiPath}/admin/product/${this.tempProduct.id}`;
-      let method = "put";
-
-      // 建立新產品
-      if (this.isNew === true) {
-        url = `${apiUrl}/api/${apiPath}/admin/product`;
-        method = "post";
-      }
-
-      axios[method](url, { data: this.tempProduct })
-        .then(() => {
-          productModal.hide();
-          this.$emit("get-products");
-        })
-        .catch((err) => alert(err));
-    },
-    createImages() {
-      this.tempProduct.imagesUrl = [];
-      this.tempProduct.imagesUrl.push("");
-    },
-    deleteImage(imageUrl) {
-      const imgsArr = this.tempProduct.imagesUrl;
-      const idx = Object.values(imgsArr).findIndex((item) => item === imageUrl);
-      this.tempProduct.imagesUrl.splice(idx, 1);
-    },
-  },
-});
-
-app.component("deleteModal", {
-  template: "#deleteModal",
-  props: ["tempProduct"],
-  mounted() {
-    delProductModal = new bootstrap.Modal(
-      document.getElementById("delProductModal")
-    );
-  },
-  methods: {
     delProduct() {
       const url = `${apiUrl}/api/${apiPath}/admin/product/${this.tempProduct.id}`;
 
       axios
         .delete(url)
         .then(() => {
-          this.hideModal();
-          this.$emit("getProducts");
+          this.getProducts();
+          this.$refs.delProductModal.closeModal();
         })
         .catch((err) => alert(err));
-    },
-    hideModal() {
-      delProductModal.hide();
     },
   },
 });
