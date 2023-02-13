fdfe60cfb727ce461700a77867b56c02188d3e3a
KumaShow <kumadth@gmail.com>
2023-02-13 15:58:35 +0800

    新增 week 6 dist 靜態檔案

---
+++ Week6/dist/assets/AboutView-699177e2.js
@@ -0,0 +1,1 @@
+import{_ as e,o as t,c}from"./index-f9f93841.js";const o={},s={class:"text-center my-3"};function n(r,_){return t(),c("h2",s,"關於我們")}const f=e(o,[["render",n]]);export{f as default};
